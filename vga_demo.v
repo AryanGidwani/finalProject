3f4c7431d5015adf53987552a078bf8569efd86e
Aryan Gidwani <95191281+AryanGidwani@users.noreply.github.com>
2024-11-18 17:38:55 -0500

    Update vga_demo.v

--- vga_demo.v
+++ vga_demo.v
@@ -3,7 +3,7 @@ module vga_demo(CLOCK_50, SW, KEY, HEX3, HEX2, HEX1, HEX0,
                 VGA_HS, VGA_VS, VGA_BLANK_N, VGA_SYNC_N, VGA_CLK, LEDR, PS2_CLK, PS2_DAT);
     
     input CLOCK_50;    
-    input [7:0] SW;
+    input [9:0] SW;
 	 output [9:0] LEDR;
     input [3:0] KEY;
     output [6:0] HEX3, HEX2, HEX1, HEX0;
@@ -16,12 +16,12 @@ module vga_demo(CLOCK_50, SW, KEY, HEX3, HEX2, HEX1, HEX0,
     output VGA_SYNC_N;
     output VGA_CLK;    
     wire [2:0] colour;
-    wire [7:0] X;
-    wire [6:0] Y;
-    assign colour = 0;
+    wire [7:0] X, X_FSM;
+    wire [6:0] Y, Y_FSM;
 	inout PS2_CLK, PS2_DAT;
 	wire start;
-	 
+	wire plot;
+	
 	 PS2_Comm comm(
         .CLOCK_50(CLOCK_50),
         .KEY(KEY[0:0]),
@@ -42,7 +42,7 @@ module vga_demo(CLOCK_50, SW, KEY, HEX3, HEX2, HEX1, HEX0,
        .colour(colour),
        .x(X),
        .y(Y),
-       .plot(~KEY[3]),
+       .plot(plot),
        .VGA_R(VGA_R),
        .VGA_G(VGA_G),
        .VGA_B(VGA_B),
@@ -62,63 +62,28 @@ module vga_demo(CLOCK_50, SW, KEY, HEX3, HEX2, HEX1, HEX0,
 	wire [1:0] count;
 	wire [1:0] VGAstate;
 	wire [3:0] state;
-	fsm U1(CLOCK_50, KEY[0], SW[3], count[0], state, VGAstate, start);
+	wire dcount, denable, doPlot, objToPlot;
+	fsm U1(CLOCK_50, KEY[0], count[0], state, VGAstate, start, dcount, denable, 0, X_FSM, Y_FSM, doPlot, objToPlot); // ~KEY[3]: doPlot, SW[9]: objToPlot
 	counter U2(CLOCK_50, KEY[0], count);
-	
-	assign LEDR[1:0] = count[1:0];
+	downCounter U3(CLOCK_50, KEY[0], dcount, denable);
+	plotObj U4(Y_FSM, X_FSM, doPlot, KEY[0], objToPlot, CLOCK_50, X, Y, plot, colour);
 	assign LEDR[9:6] = state[3:0];
 		  
 endmodule
 
-
-//module hex7seg (hex, display);
-//    input [3:0] hex;
-//    output [6:0] display;
-//
-//    reg [6:0] display;
-//
-//    /*
-//     *       0  
-//     *      ---  
-//     *     |   |
-//     *    5|   |1
-//     *     | 6 |
-//     *      ---  
-//     *     |   |
-//     *    4|   |2
-//     *     |   |
-//     *      ---  
-//     *       3  
-//     */
-//    always @ (hex)
-//        case (hex)
-//            4'h0: display = 7'b1000000;
-//            4'h1: display = 7'b1111001;
-//            4'h2: display = 7'b0100100;
-//            4'h3: display = 7'b0110000;
-//            4'h4: display = 7'b0011001;
-//            4'h5: display = 7'b0010010;
-//            4'h6: display = 7'b0000010;
-//            4'h7: display = 7'b1111000;
-//            4'h8: display = 7'b0000000;
-//            4'h9: display = 7'b0011000;
-//            4'hA: display = 7'b0001000;
-//            4'hB: display = 7'b0000011;
-//            4'hC: display = 7'b1000110;
-//            4'hD: display = 7'b0100001;
-//            4'hE: display = 7'b0000110;
-//            4'hF: display = 7'b0001110;
-//        endcase
-//endmodule
-
-
-module fsm(clock, reset, switch, count, state, VGAstate, start);
-	input clock, reset, switch, count, start;
+module fsm(clock, reset, count, state, VGAstate, start, dcount, denable, endGame, x, y, plot, objToPlot);
+	input clock, reset, count, start, dcount;
 	//T1 - Title Screen W/O Text State, T2 - Title Screen With Text Stete, gameStart - Game Start State
-	parameter T1 = 4'b0001, T2 = 4'b0010, gameStart = 4'b0011; 
+	parameter T1 = 4'b0001, T2 = 4'b0010, gameStart = 4'b0011, DrawB = 4'b0100, DrawBWait = 4'b0101, DrawP = 4'b0110, DrawPWait = 4'b0111;
+	parameter DrawH = 4'b1000, DrawHWait = 4'b1001, DrawC = 4'b1010, DrawCWait = 4'b1011, UpdateState = 4'b1100, gameEnd = 4'b1101;
 	reg [3:0] currentState, nextState;
 	output reg [3:0] state;
 	output reg [2:0] VGAstate;
+	output reg denable;
+	output reg [7:0] x;
+	output reg [6:0] y;
+	output reg plot, objToPlot;
+	input endGame;
 	
 	always@ (posedge clock)
 		begin: state_table
@@ -127,18 +92,62 @@ module fsm(clock, reset, switch, count, state, VGAstate, start);
 					nextState <= T2;
 				 else if (start == 1)
 					nextState <= gameStart;
-				
 				 else 
 					nextState <= T1;
 			
 			T2: if (count == 1) 
 					nextState <= T1;
-					
 				 else if (start == 1)
 					nextState <= gameStart;
-					
 				else
 					nextState <= T2;
+
+			gameStart: nextState <= DrawB;
+
+			DrawB: if(dcount == 1)
+						nextState <= DrawBWait;
+					else
+						nextState <= DrawB;
+
+			DrawBWait: if(dcount == 0)
+							nextState <= DrawP;
+						else
+							nextState <= DrawBWait;
+
+			DrawP: if(dcount == 1)
+						nextState <= DrawPWait;
+					else
+						nextState <= DrawP;
+
+			DrawPWait: if(dcount == 0)
+							nextState <= DrawH;
+						else
+							nextState <= DrawPWait;
+			
+			DrawH: if(dcount == 1)
+						nextState <= DrawHWait;
+					else
+						nextState <= DrawH;
+
+			DrawHWait: if(dcount == 0)
+							nextState <= DrawC;
+						else
+							nextState <= DrawHWait;
+
+			DrawC: if(dcount == 1)
+						nextState <= DrawCWait;
+					else
+						nextState <= DrawC;
+
+			DrawCWait: if(dcount == 0)
+							nextState <= UpdateState;
+						else
+							nextState <= DrawCWait;
+			
+			UpdateState: if(endGame == 1)
+							nextState <= gameEnd;
+						else
+							nextState <= DrawB;
 		endcase
 		end
 	
@@ -150,13 +159,57 @@ module fsm(clock, reset, switch, count, state, VGAstate, start);
 		else
 			currentState <= nextState;
 	end
-	
+	reg [6:0] ballY;
+	reg [7:0] hoopX;
 	always@(posedge clock)
 	begin 
 		case (currentState)
-			T1: VGAstate <= 2'b01; //Sets VGA State to Title Screen W/O Text
+			T1: begin
+				VGAstate <= 2'b01; //Sets VGA State to Title Screen W/O Text
+				denable <= 1'b0;
+				hoopX <= 0;
+				ballY <= 0; 
+			end
+		
 			T2: VGAstate <= 2'b00; //Sets VGA State to Title Screen With Text
-			gameStart: VGAstate <= 2'b10; //Sets VGA State to Game Screen
+
+			gameStart: begin
+				VGAstate <= 2'b10; //Sets VGA State to Game Screen
+				denable <= 1'b0;
+			end
+
+			DrawB: denable <= 1'b1;
+			DrawBWait:  denable <= 1'b0;
+			
+			DrawP: begin
+					 denable <= 1'b1;
+					 plot <= 0;
+					 objToPlot <= 1;
+					 x <= 8'd30;
+					 y <= 7'd100; // update module
+					 end
+			DrawPWait: begin 
+						  denable <= 1'b0;
+					     plot <= 1; // starts drawing
+					     end
+
+			DrawH: begin
+					 denable <= 1'b1;
+					 plot <= 0;
+					 objToPlot <= 0; // 0 for hoop, 1 for ball
+					 x <= hoopX; // update module
+					 y <= 7'd60;
+					 end
+			DrawHWait: begin 
+						  denable <= 1'b0;
+					     plot <= 1;
+					     end
+
+			DrawC: denable <= 1'b1;
+			DrawCWait: denable <= 1'b0;
+			
+			UpdateState: hoopX <= hoopX - 1;
+					 
 		endcase
 	end
 	
@@ -191,6 +244,154 @@ module counter(clock, reset, count);
 			count <= count + 1;
 	end
 endmodule
+
+module downCounter(clock, reset, dcount, enable);
+	input [0:0] reset;
+	input clock;
+	reg [25:0] fastcount;
+	output reg [1:0] dcount;
+	input enable;
+	
+	always@(posedge clock)
+	begin
+		if (reset[0] == 0 || enable)
+			// Resets downcounter if it gets reset or enabled
+			// Counts from 500,000 -> 0. Takes around 10ms, faster than the refresh rate of the VGA (60Hz = 16.67ms/refresh)
+			fastcount <= 26'd500000;
+		else 
+			// Every other time the downcounter is counting down
+			fastcount <= fastcount - 1'b1;
+			
+	end
+	
+	always@(posedge clock)
+	begin
+		// dcount = 0 if couting down is finished, 1 if down counter is still counting
+		if (reset[0] == 0 || enable)
+			// Resets dcount to 1 to start count down
+			dcount <= 1'b1;
+		else if (fastcount == 1'b0)
+			// Once the fastcounter is done counting, set dcount to 0
+			dcount <= 1'b0;
+	end
+endmodule
+
+module plotObj(y, x, plot, reset, ObjToPlot, CLOCK_50, VGA_X, VGA_Y, doPlot, VGA_COLOR);
+	input CLOCK_50, plot, reset;
+	output reg doPlot;
+	output reg [7:0] VGA_X;       
+	output reg [6:0] VGA_Y;
+	input [7:0] x;
+	input [6:0] y;
+	output reg [2:0] VGA_COLOR;
+	input ObjToPlot;
+
+	wire [7:0] X;           // starting x location of object
+	wire [6:0] Y;           // starting y location of object
+   wire [4:0] XC, YC;      // used to access object memory
+   wire Ex, Ey, playerEy;
+	
+	wire [2:0] VGA_Player;
+	wire [2:0] VGA_Hoop;
+	
+	
+	 wire [3:0] playerXC, playerYC; 
+	
+    // store (x,y) starting location
+    regn U1 (y, reset, 1'b1, CLOCK_50, Y);
+        defparam U1.n = 7;
+		  
+    regn U2 (x, reset, 1'b1, CLOCK_50, X);
+        defparam U2.n = 8;
+
+    count U3 (CLOCK_50, reset, Ex, XC);    // Hoop column counter
+        defparam U3.n = 5;
+    
+	 
+	 count C3 (CLOCK_50, reset, Ex, playerXC);    // player column counter
+        defparam C3.n = 4;
+		  
+	 // enable XC when VGA plotting starts  
+    regn U5 (1'b1, reset, plot, CLOCK_50, Ex);
+        defparam U5.n = 1;
+		  
+    count U4 (CLOCK_50, reset, Ey, YC);    // Hoop row counter
+        defparam U4.n = 5;
+		  
+	 count C4 (CLOCK_50, reset, playerEy, playerYC);    // player row counter
+        defparam C4.n = 4;
+		  
+    // enable Y Counters at the end of each object row
+    assign Ey = (XC == 5'b11111);
+	 assign playerEy = (playerXC == 4'b1111);
+
+    // read a pixel color from object memory
+    object_mem U6 ({YC,XC}, CLOCK_50, VGA_Hoop);
+	 player_mem U10 ({playerYC,playerXC}, CLOCK_50, VGA_Player);
+	 
+	 wire [7:0] P_VGA_X, O_VGA_X;
+	 wire [6:0] P_VGA_Y, O_VGA_Y;
+	 
+	 regn U7 (X + XC, reset, 1'b1, CLOCK_50, O_VGA_X);
+        defparam U7.n = 8;
+    regn U8 (Y + YC, reset, 1'b1, CLOCK_50, O_VGA_Y);
+        defparam U8.n = 7;
+	 
+	 regn P7 (X + playerXC, reset, 1'b1, CLOCK_50, P_VGA_X);
+        defparam P7.n = 8;
+    regn P8 (Y + playerYC, reset, 1'b1, CLOCK_50, P_VGA_Y);
+        defparam P8.n = 7;
+	 
+	 always@(negedge CLOCK_50) begin
+		if(ObjToPlot == 1'b1) begin
+			VGA_COLOR <= VGA_Player;
+			VGA_X <= P_VGA_X;
+			VGA_Y <= P_VGA_Y;
+		end
+		else begin
+			VGA_COLOR <= VGA_Hoop;
+			VGA_X <= O_VGA_X;
+			VGA_Y <= O_VGA_Y;
+		end 
+			
+	 end
+	 
+	 // store correct value of doPlot
+	 always@(VGA_COLOR) begin
+		if(VGA_COLOR == 3'b011)
+			doPlot <= 1'b0;
+		else
+			doPlot <= plot; // only plot if the colour is not blue
+	 end
+	 
+endmodule
+
+module regn(R, Resetn, E, Clock, Q);
+    parameter n = 8;
+    input [n-1:0] R;
+    input Resetn, E, Clock;
+    output reg [n-1:0] Q;
+
+    always @(posedge Clock)
+        if (!Resetn)
+            Q <= 0;
+        else if (E)
+            Q <= R;
+endmodule
+
+// counter
+module count (Clock, Resetn, E, Q);
+    parameter n = 8;
+    input Clock, Resetn, E;
+    output reg [n-1:0] Q;
+
+    always @ (posedge Clock)
+        if (Resetn == 0)
+            Q <= 0;
+        else if (E)
+            Q <= Q + 1;
+endmodule
+
 
 
 
