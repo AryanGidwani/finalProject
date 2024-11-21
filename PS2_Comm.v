5d6acb86f5071ae43e4e796ab107f5f034e9b11c
Harsh Patel <92759695+harshhmp@users.noreply.github.com>
2024-11-20 19:07:08 -0500

    Update PS2_Comm.v

--- PS2_Comm.v
+++ PS2_Comm.v
@@ -12,7 +12,8 @@ module PS2_Comm(
     // outputs
     HEX0,
     HEX1,
-	 start
+	 start,
+	 moveUp
 
 );
 
@@ -113,8 +114,8 @@ PS2_Controller PS2 (
 	.received_data_en	(ps2_key_pressed)
 );
 
-output start; //Start game signal
-Hex_To_Letter Segment0 (CLOCK_50, last_data_received, HEX0, start);
+output start, moveUp; //Start game signal
+Hex_To_Letter Segment0 (CLOCK_50, last_data_received, HEX0, start, moveUp);
 
 
 endmodule
@@ -125,7 +126,7 @@ module Hex_To_Letter(clock, last_data_received, out, start);
 	output reg [6:0] out;
 	output reg start;
 
-	parameter [7:0] W = 7'h1D, A = 7'h1C, S = 7'h1B, D = 7'h23, P = 7'h4D;
+	parameter [7:0] W = 8'h1D, A = 8'h1C, S = 8'h1B, D = 8'h23, P = 8'h4D;
 	always@(posedge clock) begin
 		case(last_data_received)
 			W: out <= 7'b0000000;
@@ -133,10 +134,72 @@ module Hex_To_Letter(clock, last_data_received, out, start);
 			S: out <= 7'b0010010;
 			D: out <= 7'b0100001;
 			P: start <= 1;
-			default: start <= 0;
+			Space: moveUp <= 1;
+			default: begin 
+			start <= 0;
+			end
 		endcase
 	end
 
 
 
+endmodule
+
+module PS2FSM(clock, last_data_received, out, moveUp);
+	input clock;
+	input wire [7:0] last_data_received;
+	output reg [6:0] out;
+	output reg moveUp;
+	reg [3:0] currentState, nextState;
+	parameter [3:0] S1 = 4'b0000, S2 = 4'b0001, S3 = 4'b0010, S4 = 4'b0011, S5 = 4'b0100; 
+	parameter [7:0] W = 8'h1D, A = 8'h1C, S = 8'h1B, D = 8'h23, P = 8'h4D, Space = 8'h29, F = 8'hF0;
+	
+	always@ (posedge clock)
+		begin: state_table
+		case (currentState)
+			S1: if(last_data_received == Space) 
+					nextState <= S2;
+				 else if(last_data_received == F)
+					nextState <= S4;
+					
+			S2: if(last_data_received == Space) 
+					nextState <= S2;
+				 else if(last_data_received == F)
+					nextState <= S3;
+					
+			S3: if(last_data_received == Space) 
+					nextState <= S5;
+				 else if(last_data_received == F)
+					nextState <= S3;
+			
+			S4: if(last_data_received == Space) 
+					nextState <= S5;
+				 else if(last_data_received == F)
+					nextState <= S4;
+			
+			S5: if(last_data_received == Space) 
+					nextState <= S2;
+				 else if(last_data_received == F)
+					nextState <= S3;
+			default: nextState <= S1;
+		endcase
+		end
+		
+		always @(posedge clock)
+		begin: state_FFs
+			state <= currentState;
+			if (!reset)
+				currentState <= T1;
+			else
+				currentState <= nextState;
+		end
+		
+		always@(posedge clock)
+		begin 
+		case (currentState)
+			S1: moveUp <= 0;
+			S3: moveUp <= 1;
+			S5: moveUp <= 0;
+		endcase
+		end
 endmodule
